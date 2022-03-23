26eb5adce0d38f99032370d3f3d752faa011b07b
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2022-03-23 19:02:52 -0300

    views de cadastro

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import UserCreateForm
+
+class SignUpCreateView(CreateView):
+    form_class = UserCreateForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
